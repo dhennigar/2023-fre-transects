ade9fc2ccb0cf063848aeb5fc2af447846f10d49
dhennigar <danrhennigar@gmail.com>
2023-09-13 13:51:30 -0700

    add flora

--- scripts/data-import.R
+++ scripts/data-import.R
@@ -6,6 +6,8 @@ library(stringr)
 library(dplyr)
 library(janitor)
 
+## Transect data import and cleaning
+
 files <- paste("./data/", list.files("./data"), sep = "")
 transects <- data.frame()
 
@@ -24,3 +26,14 @@ transects <- transects %>%
     clean_names()
 
 saveRDS(transects, "./data/combined_transects.RDS")
+
+
+## Floristic data import and cleaning
+
+flora_raw <- read.csv("./data/species_lists/floristic_lists.csv")
+
+flora <- flora_raw %>%
+    clean_names() %>%
+    select(c(site_name, species, low_marsh_only))
+
+saveRDS(flora, "./data/floristic_surveys.RDS")
